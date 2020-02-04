838cda99b7773da7bffd85217f2dd59917827eb4
EugenePetrik <eugeniy.petrik@gmail.com>
2020-02-04 13:08:08 +0200

    Initial commit

---
+++ spec/support/config/capybara.rb
@@ -0,0 +1,37 @@
+require 'capybara/rails'
+require 'webdrivers/chromedriver'
+
+Capybara.register_driver :chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    loggingPrefs: {
+      browser: 'ALL'
+    }
+  )
+
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('window-size=1600,1268')
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: capabilities,
+    options: options
+  )
+end
+
+# Capybara.register_driver :headless_chrome do |app|
+#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+#     chromeOptions: { args: %w(headless disable-gpu) },
+#     loggingPrefs: logging_preferences
+#   )
+#   Capybara::Selenium::Driver.new(
+#     app,
+#     browser: :chrome,
+#     desired_capabilities: capabilities
+#   )
+# end
+
+Capybara.default_max_wait_time = 3
+Capybara.javascript_driver = :chrome
+Capybara.default_driver = :chrome
+Capybara.server = :puma
